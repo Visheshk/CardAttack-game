6e799772038bb581a8914056379ccf911ac37fa1
sjulson <sjulson@ad.engr.wisc.edu>
2019-03-21 20:18:12 -0500

    Added code base
    Signed-off-by: sjulson

---
+++ Assets/Scripts/GameVariables.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameVariables
+{
+    public static int TIME_TO_ANSWER = 5; // Units are in seconds
+    public static int SEQUENCE_LENGTH = 5;
+    public static int COMBO_LENGTH = 3;
+    public static int INITIAL_HITPOINTS = 20;
+    public static int NORMAL_DAMAGE = 1;
+    public static int FINISHER_DAMAGE = 3;
+}
