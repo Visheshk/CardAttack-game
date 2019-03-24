18d2a706b6182f0d285079b7538fca8cc339c8dc
sjulson <sjulson@ad.engr.wisc.edu>
2019-03-23 21:59:55 -0500

    Revert "Reverted for real this time"
    This reverts commit 48a928d16049167d5fa4bbe955a4104c97794ce6.

---
+++ Assets/Scripts/ComboDisplayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboDisplayer : MonoBehaviour
+{   
+    [System.Serializable]
+    public struct symbolImage {
+        public string name;
+        public Sprite image;
+    }
+    public symbolImage[] symbolImagesDict;
+    public List<GameObject> symbols;
+    // public Dictionary<String, Sprite> symbolImagesDict;
+    public List<Sprite> symbolImages;
+
+    public Sequence thisCombo;
+    // public GameObject symbol1;
+    // public GameObject symbol1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DisplayCombo(Sequence combo) {
+        thisCombo = combo;
+        for (int i = 0; i < thisCombo.getLength(); i++) {
+            // Debug.Log(thisCombo.getSymbol(i));
+            // Debug.Log( (int) thisCombo.getSymbol(i));
+            symbols[i].GetComponent<UnityEngine.UI.Image>().sprite = symbolImages[((int) thisCombo.getSymbol(i) - 1)];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
